874bfad14dc08c2c90b7b46770e821a0acdf0fd6
ahmetbucko <ahmetbucko@gmail.com>
2017-05-04 09:40:53 +0200

    Add files via upload

---
+++ CrackForma.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace ProjektiFinal_DEScrack_Siguri
+{
+    partial class CrackForma
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CrackForma));
+            this.txtFajlli = new System.Windows.Forms.TextBox();
+            this.txtRezultati = new System.Windows.Forms.TextBox();
+            this.btnFajlli = new System.Windows.Forms.Button();
+            this.btnKerko = new System.Windows.Forms.Button();
+            this.btnMbylle = new System.Windows.Forms.Button();
+            this.btnRuaj = new System.Windows.Forms.Button();
+            this.btnDekripto = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtFajlli
+            // 
+            this.txtFajlli.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtFajlli.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtFajlli.Location = new System.Drawing.Point(29, 23);
+            this.txtFajlli.Multiline = true;
+            this.txtFajlli.Name = "txtFajlli";
+            this.txtFajlli.Size = new System.Drawing.Size(233, 331);
+            this.txtFajlli.TabIndex = 1;
+            // 
+            // txtRezultati
+            // 
+            this.txtRezultati.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtRezultati.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRezultati.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.txtRezultati.Location = new System.Drawing.Point(267, 323);
+            this.txtRezultati.Multiline = true;
+            this.txtRezultati.Name = "txtRezultati";
+            this.txtRezultati.Size = new System.Drawing.Size(198, 31);
+            this.txtRezultati.TabIndex = 5;
+            // 
+            // btnFajlli
+            // 
+            this.btnFajlli.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnFajlli.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnFajlli.FlatAppearance.BorderSize = 0;
+            this.btnFajlli.Font = new System.Drawing.Font("Gill Sans MT", 10F);
+            this.btnFajlli.Location = new System.Drawing.Point(267, 23);
+            this.btnFajlli.Name = "btnFajlli";
+            this.btnFajlli.Size = new System.Drawing.Size(95, 33);
+            this.btnFajlli.TabIndex = 6;
+            this.btnFajlli.Text = "Lexo";
+            this.btnFajlli.UseVisualStyleBackColor = false;
+            this.btnFajlli.Click += new System.EventHandler(this.btnFajlli_Click);
+            // 
+            // btnKerko
+            // 
+            this.btnKerko.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnKerko.Font = new System.Drawing.Font("Gill Sans MT", 10F);
+            this.btnKerko.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnKerko.Location = new System.Drawing.Point(368, 23);
+            this.btnKerko.Name = "btnKerko";
+            this.btnKerko.Size = new System.Drawing.Size(95, 73);
+            this.btnKerko.TabIndex = 7;
+            this.btnKerko.Text = "Kerko";
+            this.btnKerko.UseVisualStyleBackColor = false;
+            this.btnKerko.Click += new System.EventHandler(this.btnKerko_Click);
+            this.btnKerko.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnKerko_Click);
+            // 
+            // btnMbylle
+            // 
+            this.btnMbylle.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnMbylle.Font = new System.Drawing.Font("Gill Sans MT", 10F);
+            this.btnMbylle.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnMbylle.Location = new System.Drawing.Point(267, 145);
+            this.btnMbylle.Name = "btnMbylle";
+            this.btnMbylle.Size = new System.Drawing.Size(196, 33);
+            this.btnMbylle.TabIndex = 8;
+            this.btnMbylle.Text = "Mbyll";
+            this.btnMbylle.UseVisualStyleBackColor = false;
+            this.btnMbylle.Click += new System.EventHandler(this.btnMbylle_Click);
+            // 
+            // btnRuaj
+            // 
+            this.btnRuaj.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnRuaj.Font = new System.Drawing.Font("Gill Sans MT", 10F);
+            this.btnRuaj.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnRuaj.Location = new System.Drawing.Point(267, 63);
+            this.btnRuaj.Name = "btnRuaj";
+            this.btnRuaj.Size = new System.Drawing.Size(95, 33);
+            this.btnRuaj.TabIndex = 9;
+            this.btnRuaj.Text = "Ruaj";
+            this.btnRuaj.UseVisualStyleBackColor = false;
+            this.btnRuaj.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnDekripto
+            // 
+            this.btnDekripto.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnDekripto.Font = new System.Drawing.Font("Gill Sans MT", 10F);
+            this.btnDekripto.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnDekripto.Location = new System.Drawing.Point(267, 102);
+            this.btnDekripto.Name = "btnDekripto";
+            this.btnDekripto.Size = new System.Drawing.Size(196, 33);
+            this.btnDekripto.TabIndex = 12;
+            this.btnDekripto.Text = "Dekripto";
+            this.btnDekripto.UseVisualStyleBackColor = false;
+            this.btnDekripto.Click += new System.EventHandler(this.btnDekripto_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(264, 188);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 117);
+            this.label1.TabIndex = 13;
+            this.label1.Text = resources.GetString("label1.Text");
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // CrackForma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(495, 385);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDekripto);
+            this.Controls.Add(this.btnRuaj);
+            this.Controls.Add(this.btnMbylle);
+            this.Controls.Add(this.btnKerko);
+            this.Controls.Add(this.btnFajlli);
+            this.Controls.Add(this.txtRezultati);
+            this.Controls.Add(this.txtFajlli);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "CrackForma";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "v";
+            this.Load += new System.EventHandler(this.CrackForma_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFajlli;
+        private System.Windows.Forms.TextBox txtRezultati;
+        private System.Windows.Forms.Button btnFajlli;
+        private System.Windows.Forms.Button btnKerko;
+        private System.Windows.Forms.Button btnMbylle;
+        private System.Windows.Forms.Button btnRuaj;
+        private System.Windows.Forms.Button btnDekripto;
+        private System.Windows.Forms.Label label1;
+    }
+}
