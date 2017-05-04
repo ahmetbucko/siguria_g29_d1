c5dbc5673878d73aa4fe18f0f37d93e5085917bf
ahmetbucko <ahmetbucko@gmail.com>
2017-05-04 09:29:30 +0200

    Add files via upload
    using System;
    using System.Collections.Generic;
    using System.ComponentModel;
    using System.Data;
    using System.Drawing;
    using System.Linq;
    using System.Text;
    using System.Threading.Tasks;
    using System.Windows.Forms;
    
    namespace ProjektiFinal_DEScrack_Siguri
    {
        public partial class frmHome : Form
        {
            public frmHome()
            {
                InitializeComponent();
            }
            CrackForma frmCrac = new CrackForma();
            FormaKryesore frmKry = new FormaKryesore();
            FormaDekriptimi frmDec = new FormaDekriptimi();
    
            private void btnEkripto_Click(object sender, EventArgs e)
            {
                frmKry.Show();
            }
    
            private void btnCrack_Click(object sender, EventArgs e)
            {
                //frmCrac.Show();
            }
    
            private void btnDekripto_Click(object sender, EventArgs e)
            {
                frmDec.Show();
            }
    
            private void dESECBEncryptionToolStripMenuItem_Click(object sender, EventA

---
+++ frmHome.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjektiFinal_DEScrack_Siguri
+{
+    public partial class frmHome : Form
+    {
+        public frmHome()
+        {
+            InitializeComponent();
+        }
+        CrackForma frmCrac = new CrackForma();
+        FormaKryesore frmKry = new FormaKryesore();
+        FormaDekriptimi frmDec = new FormaDekriptimi();
+
+        private void btnEkripto_Click(object sender, EventArgs e)
+        {
+            frmKry.Show();
+        }
+
+        private void btnCrack_Click(object sender, EventArgs e)
+        {
+            //frmCrac.Show();
+        }
+
+        private void btnDekripto_Click(object sender, EventArgs e)
+        {
+            frmDec.Show();
+        }
+
+        private void dESECBEncryptionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormaKryesore enc = new FormaKryesore();
+            //enc.ShowDialog();
+            enc.Show();
+        }
+
+        private void dESECBDecryptionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormaDekriptimi dec = new FormaDekriptimi();
+            dec.ShowDialog();
+        }
+
+        private void dESDictionaryAttackToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CrackForma frmC = new CrackForma();
+            
+            frmC.ShowDialog();
+            //this.Hide();
+        }
+
+        private void programInfoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            prgInfo info = new prgInfo();
+            info.ShowDialog();
+        }
+
+        private void contactUsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            contactUs cnt = new contactUs();
+            cnt.ShowDialog();
+        }
+
+        private void frmHome_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
