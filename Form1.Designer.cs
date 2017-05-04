56f2cfce2d28bd9a9e234a2e78418e25a1857588
ahmetbucko <ahmetbucko@gmail.com>
2017-05-04 09:40:07 +0200

    Add files via upload
    using System;
    using System.Collections.Generic;
    using System.ComponentModel;
    using System.Data;
    using System.Drawing;
    using System.Linq;
    using System.Text;
    using System.Threading.Tasks;
    using System.Windows.Forms;
    using System.Security.Cryptography;
    using System.IO;
    
    namespace ProjektiFinal_DEScrack_Siguri
    {
        public partial class FormaKryesore : Form
        {
            public FormaKryesore()
            {
                InitializeComponent();
            }
            public static string Encrypt(string message, string password)
            {
                byte[] messageBytes = ASCIIEncoding.ASCII.GetBytes(message);
                byte[] passwordBytes = ASCIIEncoding.ASCII.GetBytes(password);
    
                DESCryptoServiceProvider provider = new DESCryptoServiceProvider();
                ICryptoTransform transform = provider.CreateEncryptor(passwordBytes, passwordBytes);//Symetric encryptor object with current key and IV
                CryptoStreamMode mode = CryptoStreamMode.Write;

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace ProjektiFinal_DEScrack_Siguri
+{
+    partial class FormaKryesore
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormaKryesore));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUnecrypted = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEncrypted = new System.Windows.Forms.TextBox();
+            this.btnEnkriptimi = new System.Windows.Forms.Button();
+            this.lblClose = new System.Windows.Forms.Label();
+            this.lblMinimize = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnSaveEncryptenText = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Courier New", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(78, 94);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(277, 23);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Plain Text";
+            // 
+            // txtUnecrypted
+            // 
+            this.txtUnecrypted.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtUnecrypted.BackColor = System.Drawing.Color.White;
+            this.txtUnecrypted.Font = new System.Drawing.Font("Courier New", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUnecrypted.ForeColor = System.Drawing.Color.DarkGreen;
+            this.txtUnecrypted.Location = new System.Drawing.Point(82, 115);
+            this.txtUnecrypted.Margin = new System.Windows.Forms.Padding(4);
+            this.txtUnecrypted.Multiline = true;
+            this.txtUnecrypted.Name = "txtUnecrypted";
+            this.txtUnecrypted.Size = new System.Drawing.Size(832, 37);
+            this.txtUnecrypted.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Courier New", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(78, 174);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 23);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtPassword.BackColor = System.Drawing.Color.White;
+            this.txtPassword.Font = new System.Drawing.Font("Courier New", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.ForeColor = System.Drawing.Color.Red;
+            this.txtPassword.Location = new System.Drawing.Point(82, 194);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPassword.Multiline = true;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(832, 37);
+            this.txtPassword.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Courier New", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(78, 252);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(252, 23);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Encrypted Text";
+            // 
+            // txtEncrypted
+            // 
+            this.txtEncrypted.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtEncrypted.BackColor = System.Drawing.Color.White;
+            this.txtEncrypted.Font = new System.Drawing.Font("Courier New", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEncrypted.ForeColor = System.Drawing.Color.Blue;
+            this.txtEncrypted.Location = new System.Drawing.Point(82, 273);
+            this.txtEncrypted.Margin = new System.Windows.Forms.Padding(4);
+            this.txtEncrypted.Multiline = true;
+            this.txtEncrypted.Name = "txtEncrypted";
+            this.txtEncrypted.Size = new System.Drawing.Size(832, 37);
+            this.txtEncrypted.TabIndex = 15;
+            // 
+            // btnEnkriptimi
+            // 
+            this.btnEnkriptimi.AutoSize = true;
+            this.btnEnkriptimi.BackColor = System.Drawing.Color.White;
+            this.btnEnkriptimi.Font = new System.Drawing.Font("Lucida Bright", 11.25F);
+            this.btnEnkriptimi.Location = new System.Drawing.Point(762, 318);
+            this.btnEnkriptimi.Margin = new System.Windows.Forms.Padding(4, 4, 1, 6);
+            this.btnEnkriptimi.Name = "btnEnkriptimi";
+            this.btnEnkriptimi.Size = new System.Drawing.Size(151, 42);
+            this.btnEnkriptimi.TabIndex = 18;
+            this.btnEnkriptimi.Text = "Encrypt";
+            this.btnEnkriptimi.UseVisualStyleBackColor = false;
+            this.btnEnkriptimi.Click += new System.EventHandler(this.btnEnkriptimi_Click);
+            // 
+            // lblClose
+            // 
+            this.lblClose.AutoSize = true;
+            this.lblClose.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClose.ForeColor = System.Drawing.Color.Black;
+            this.lblClose.Location = new System.Drawing.Point(952, 9);
+            this.lblClose.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblClose.Name = "lblClose";
+            this.lblClose.Size = new System.Drawing.Size(35, 34);
+            this.lblClose.TabIndex = 20;
+            this.lblClose.Text = "X";
+            this.lblClose.Click += new System.EventHandler(this.lblClose_Click);
+            // 
+            // lblMinimize
+            // 
+            this.lblMinimize.AutoSize = true;
+            this.lblMinimize.Font = new System.Drawing.Font("Wide Latin", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMinimize.ForeColor = System.Drawing.Color.Black;
+            this.lblMinimize.Location = new System.Drawing.Point(912, 10);
+            this.lblMinimize.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMinimize.Name = "lblMinimize";
+            this.lblMinimize.Size = new System.Drawing.Size(30, 33);
+            this.lblMinimize.TabIndex = 21;
+            this.lblMinimize.Text = "_";
+            this.lblMinimize.Click += new System.EventHandler(this.lblMinimize_Click);
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Lucida Bright", 11.25F);
+            this.button1.Location = new System.Drawing.Point(542, 318);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 1, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 42);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Brute-Force";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSaveEncryptenText
+            // 
+            this.btnSaveEncryptenText.BackColor = System.Drawing.Color.Transparent;
+            this.btnSaveEncryptenText.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnSaveEncryptenText.BackgroundImage")));
+            this.btnSaveEncryptenText.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnSaveEncryptenText.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Italic);
+            this.btnSaveEncryptenText.Location = new System.Drawing.Point(698, 317);
+            this.btnSaveEncryptenText.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSaveEncryptenText.Name = "btnSaveEncryptenText";
+            this.btnSaveEncryptenText.Size = new System.Drawing.Size(60, 43);
+            this.btnSaveEncryptenText.TabIndex = 19;
+            this.btnSaveEncryptenText.UseVisualStyleBackColor = false;
+            this.btnSaveEncryptenText.Click += new System.EventHandler(this.btnSaveEncryptenText_Click);
+            // 
+            // FormaKryesore
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1000, 400);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblMinimize);
+            this.Controls.Add(this.lblClose);
+            this.Controls.Add(this.btnSaveEncryptenText);
+            this.Controls.Add(this.btnEnkriptimi);
+            this.Controls.Add(this.txtEncrypted);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUnecrypted);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FormaKryesore";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Enkriptimi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUnecrypted;
+        private System.Windows.Forms.Label label2;
+        public System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnEnkriptimi;
+        public System.Windows.Forms.TextBox txtEncrypted;
+        private System.Windows.Forms.Button btnSaveEncryptenText;
+        private System.Windows.Forms.Label lblClose;
+        private System.Windows.Forms.Label lblMinimize;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
